60b44b7412e240c2ac0b7bb28a7b95aeec3651f8
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:50:49 +0300

    change

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+
+@app.route('/shop')
+def my_shop():
+	products = ["lipsticks", "mascaras","eyeshadows"]
+	return render_template(
+		"shop.html" ,
+		products=products)
 
 if __name__ == '__main__':
    app.run(debug = True)
